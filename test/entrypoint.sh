520c637aec789ebc6a98f637c9ff2bde05f72ce4
Chris Stefano <virtualstaticvoid@gmail.com>
2025-01-12 19:05:31 +0000

    improve test script

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -22,36 +22,47 @@ if [ ! -z ${BUILDPACK_TEST_DEBUG+x} ] && [ "$BUILDPACK_TEST_DEBUG" == "1" ]; the
   trap "debug_break" ERR
 fi
 
+
 # ensure expected directories
 mkdir -p $BUILDPACK_DIR $BUILD_DIR $CACHE_DIR $ENV_DIR
 
-# enter buildpack directory (Heroku does this step)
-# and clone the buildpack sources
+# get buildpack sources
 echo
-echo "Cloning ${BUILDPACK_CLONE_URL}#${BUILDPACK_BRANCH}..."
-cd $BUILDPACK_DIR
-git clone --branch $BUILDPACK_BRANCH --depth=1 --quiet $BUILDPACK_CLONE_URL .
-echo "Using version: $(git rev-parse HEAD)"
+(
+  cd $BUILDPACK_DIR
+  git config --global --add safe.directory $(pwd)
+
+  if [ ! -d .git ]; then
+    # clone the buildpack sources
+    echo "Cloning ${BUILDPACK_CLONE_URL}#${BUILDPACK_BRANCH}..."
+    git clone --branch $BUILDPACK_BRANCH --depth=1 --quiet $BUILDPACK_CLONE_URL .
+  fi
+  echo "Using version: $(git rev-parse HEAD)"
+)
 
 # enter build directory (which is working directory during slug compilation)
-cd $BUILD_DIR
-
-# emulate detection
 echo
-echo "Detection..."
-$BUILDPACK_DIR/bin/detect "$BUILD_DIR"
+(
+  cd $BUILD_DIR
 
-# emulate slug compilation
-echo
-debug_break "Slug compilation..."
-$BUILDPACK_DIR/bin/compile "$BUILD_DIR" "$CACHE_DIR" "$ENV_DIR"
+  # emulate detection
+  echo "Detection..."
+  $BUILDPACK_DIR/bin/detect "$BUILD_DIR"
+
+  # emulate slug compilation
+  debug_break "Slug compilation..."
+  $BUILDPACK_DIR/bin/compile "$BUILD_DIR" "$CACHE_DIR" "$ENV_DIR"
+)
 
 # set the environment from compile outputs
 source $BUILD_DIR/.profile.d/heroku-buildpack-r-env.sh
 
-cd $APP_DIR
-
-# run the provided test script
+# run tests
 echo
-debug_break "Testing application..."
-./test.sh
+(
+  cd $APP_DIR
+
+  # run the provided test script
+  debug_break "Testing application..."
+  ./test.sh
+)
